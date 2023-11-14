4b5340ecac77ac1064e5d86b40428d8d5271c194
Brando <bbaker.sh@gmail.com>
2023-11-13 17:52:01 -0800

    init commit, completed exercise

---
+++ letConst/main.js
@@ -0,0 +1,16 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+let PI = 3.14;
+PI = 42;
+/*
+
+- What is the difference between ***var*** and ***let***?
+    let is block scoped to where its referenced. Also, It doesn't added the var to the window object. Lastly, you can't re-declare variables
+- What is the difference between **var** and ***const***?
+    You can't redeclare or reassign const, const is block scoped, and can't be hoisted
+- What is the difference between **let** and **const**?
+    You can't redeclare or reassign const. But you can reassign with let
+- What is hoisting?
+    Basically you can use var later in the global scope even if it was called lower in the code. This ain't it tbh.
+*/
